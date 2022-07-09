40b96427973143eba3dc91e88a086f51214c2ece
Agustlescano <agustinlescano37@gmail.com>
2022-07-09 11:55:46 -0300

    Auth0 ready

--- henry-diamond/src/index.js
+++ henry-diamond/src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
+import { Auth0Provider } from "@auth0/auth0-react";
 import store from './store/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
+        <Auth0Provider
+        domain="dev-j62go36g.us.auth0.com"
+        clientId="699duSJquLgBQ2pD3pC07dDh5vLMs8k8"
+        redirectUri={window.location.origin}
+        >
+
     <App />
+        </Auth0Provider>
+
     </Provider>
 
 );
