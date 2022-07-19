9af2503f2d5fc3f3e4d0287c765230e1982e4dfb
unknown <Lautaro Ferreyra>
2022-07-19 08:10:45 -0300

    trabajando con provider auth0

--- henry-diamond/src/components/Profile/Profile.jsx
+++ henry-diamond/src/components/Profile/Profile.jsx
@@ -2,12 +2,29 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { LogoutButton } from "../Logout/Logout";
 import './Profile.css'
 import React, {useEffect, useState}from "react";
-
-
+import { addUser } from "../../actions/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const dispatch = useDispatch();
 
+  const [newUser,setNewUser] = useState({
+    id : "",
+    newUser: "",
+    email: ""
+});
+
+
+  if (user){
+    setNewUser({
+      ...newUser,
+      id : user.sub,
+      newUser: user.name,
+      email: user.email
+    })
+    dispatch(addUser(newUser))
+  }
 
   if (isLoading) {
     return <div>Loading...</div>;
--- henry-diamond/src/index.js
+++ henry-diamond/src/index.js
@@ -4,18 +4,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-// import { Auth0Provider } from "@auth0/auth0-react"; //cambiamos a un provider con historial de navegación vvvvvvvvvvvv
-import Auth0ProviderWithHistory from './auth/auth0-provider-width-history';
+import { Auth0Provider } from "@auth0/auth0-react"; //cambiamos a un provider con historial de navegación vvvvvvvvvvvv
+//import Auth0ProviderWithHistory from './auth/auth0-provider-width-history';
 import store from './store/store.js';
 import { BrowserRouter } from 'react-router-dom';
-
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <Auth0ProviderWithHistory>
+                <Auth0Provider
+                domain={domain}
+                clientId={clientId}
+                audience={audience}
+                cacheLocation="localstorage"
+                >
                     <App />
-                </Auth0ProviderWithHistory>
+                </Auth0Provider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
