f2c721a17093804d69bfa759bc6d21bbb540401b
unknown <Lautaro Ferreyra>
2022-07-19 17:14:43 -0300

    cambios a trabajando con auth0

--- henry-diamond/src/index.js
+++ henry-diamond/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { Auth0Provider } from "@auth0/auth0-react"; //cambiamos a un provider con historial de navegación vvvvvvvvvvvv
-//import Auth0ProviderWithHistory from './auth/auth0-provider-width-history';
+import { Auth0Provider } from "@auth0/auth0-react";
 import store from './store/store.js';
 import { BrowserRouter } from 'react-router-dom';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
