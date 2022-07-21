efadd0eba77fd04936a1a38bf33db554249edd61
unknown <Lautaro Ferreyra>
2022-07-21 10:01:39 -0300

    probando sin env de vercel

--- henry-diamond/src/index.js
+++ henry-diamond/src/index.js
@@ -7,15 +7,18 @@ import { Provider } from 'react-redux';
 import { Auth0Provider } from "@auth0/auth0-react";
 import store from './store/store.js';
 import { BrowserRouter } from 'react-router-dom';
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+/*const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;*/
+/*REACT_APP_AUTH0_DOMAIN=dev-qkovfz2t.us.auth0.com
+REACT_APP_AUTH0_CLIENT_ID=MLEfWPyhmbMJI7btV70HIyykJlB1lex1
+REACT_APP_AUTH0_AUDIENCE=https://express.sample/
+REACT_APP_SERVER_URL=http://localhost:3000/ */
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <Auth0Provider
-            domain={domain}
-            clientId={clientId}
+            domain="dev-qkovfz2t.us.auth0.com"
+            clientId="MLEfWPyhmbMJI7btV70HIyykJlB1lex1"
             redirectUri={window.location.origin}
             cacheLocation="localstorage">
                 <BrowserRouter>
