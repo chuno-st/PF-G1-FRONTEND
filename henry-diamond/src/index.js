dc8150686afe5901ee084e981641d6d490f34cb2
unknown <Lautaro Ferreyra>
2022-07-21 10:29:35 -0300

    regularizando env

--- henry-diamond/src/index.js
+++ henry-diamond/src/index.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux';
 import { Auth0Provider } from "@auth0/auth0-react";
 import store from './store/store.js';
 import { BrowserRouter } from 'react-router-dom';
-/*const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;*/
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 /*REACT_APP_AUTH0_DOMAIN=dev-qkovfz2t.us.auth0.com
 REACT_APP_AUTH0_CLIENT_ID=MLEfWPyhmbMJI7btV70HIyykJlB1lex1
 REACT_APP_AUTH0_AUDIENCE=https://express.sample/
@@ -17,8 +17,8 @@ ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <Auth0Provider
-            domain="dev-qkovfz2t.us.auth0.com"
-            clientId="MLEfWPyhmbMJI7btV70HIyykJlB1lex1"
+            domain={domain}
+            clientId={clientId}
             redirectUri={window.location.origin}
             cacheLocation="localstorage">
                 <BrowserRouter>
