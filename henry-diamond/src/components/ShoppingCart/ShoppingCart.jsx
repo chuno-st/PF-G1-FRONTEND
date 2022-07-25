f574e1195acf2fa739394b335423891f77ecc33a
chuno-st <arq.stauber@gmail.com>
2022-07-25 11:55:51 -0300

    pruebas con token

--- henry-diamond/src/components/ShoppingCart/ShoppingCart.jsx
+++ henry-diamond/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import {useSelector,useDispatch} from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import CardCart from "../CardCart/CardCart";
 import { Button } from "@material-ui/core";
@@ -7,31 +7,58 @@ import Typography from "@material-ui/core/Typography";
 import { postCart } from "../../actions/actions";
 import { useAuth0 } from "@auth0/auth0-react";
 
-export default function ShoppingCart(){
+export default function ShoppingCart() {
 
-    const { user } = useAuth0();
+  const { user } = useAuth0();
 
-    const dispatch = useDispatch()
-    const miStorage = window.localStorage;
-    let Productos = Object.values(miStorage)
-    let objetos = Productos.map(producto => {return JSON.parse(producto)})
-    let productos = objetos.filter(producto => producto.hasOwnProperty('product_id'))
-    console.log(productos.reduce( (acc,producto) =>acc+producto.cantidad,0))
-    const priceTotal= productos.reduce( (acc,producto) =>acc+producto.price,0)
-    const subTotal = productos.map((producto)=>producto.price*producto.cantidad)
+  const dispatch = useDispatch()
+  const miStorage = window.localStorage;
+  let Productos = Object.values(miStorage)
+  let objetos = Productos.map(producto => { return JSON.parse(producto) })
+  let productos = objetos.filter(producto => producto.hasOwnProperty('product_id'))
+  console.log(productos.reduce((acc, producto) => acc + producto.cantidad, 0))
+  const priceTotal = productos.reduce((acc, producto) => acc + producto.price, 0)
+  const subTotal = productos.map((producto) => producto.price * producto.cantidad)
 
-    const handlerSubmit = () =>{
-        dispatch(postCart(productos, user.sub))
+  const handlerSubmit = () => {
+    dispatch(postCart(productos, user.sub))
+  }
+
+  const [message, setMessage] = useState('');
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  const callSecureApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(
+        `${serverUrl}payment/button`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const responseData = await response.json();
+
+      setMessage(responseData.message);
+    } catch (error) {
+      setMessage(error.message);
     }
+  };
 
-    return (
+  return (
     <>
-    <h4>usted esta aqui</h4>
-    {
-        productos.map(producto => {return <CardCart item={producto}/> })
-} 
-    <Typography variant="h6" gutterBottom> Total: {subTotal.reduce( (acc,producto) =>acc+producto,0)}</Typography>
-    <Button variant="contained" color="primary" onClick={handlerSubmit} >  Comprar</Button>
+      <h4>usted esta aqui</h4>
+      {
+        productos.map(producto => { return <CardCart item={producto} /> })
+      }
+      <Typography variant="h6" gutterBottom> Total: {subTotal.reduce((acc, producto) => acc + producto, 0)}</Typography>
+      <Button variant="contained" color="primary" onClick={handlerSubmit} >  Comprar</Button>
+      <Button variant="contained" color="primary" onClick={callSecureApi} >  Permisos</Button>
     </>
-)
+  )
 }
