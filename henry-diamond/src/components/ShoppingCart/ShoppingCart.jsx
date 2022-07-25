1d379b484b94edaa80f86256a780c5a044ec8316
soledadpetrino <93164871+soledadpetrino@users.noreply.github.com>
2022-07-25 12:28:34 -0300

    agregando estilo

--- henry-diamond/src/components/ShoppingCart/ShoppingCart.jsx
+++ henry-diamond/src/components/ShoppingCart/ShoppingCart.jsx
@@ -5,6 +5,9 @@ import CardCart from "../CardCart/CardCart";
 import NavMyAccount from '../MyAccount/NavMyAccount'
 import { ThemeProvider } from "@material-ui/core"
 import { createTheme } from "@material-ui/core";
+import { Grid, Button} from "@mui/material";
+import Footer from "../Footer/Footer";
+
 
 const theme = createTheme({
     palette: {
@@ -31,15 +34,17 @@ export default function ShoppingCart(){
     <>
     <ThemeProvider theme={theme}>
         <NavMyAccount/>
-
+        <Grid container xs={12} sm={8} md={6} lg={4}>
         {
         objetos.map(producto => {
             if (producto.hasOwnProperty('product_id')){
                 console.log(producto)
-                return <CardCart item={producto}/>
+                return <CardCart key={producto.product_id} item={producto}/>
             }else return
         })
         }
+        </Grid>
+        <Footer />
     </ThemeProvider>
 
     </>
