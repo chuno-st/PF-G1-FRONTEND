806c2893306e58422cbb662b6ff90cbdbfaa564d
unknown <Lautaro Ferreyra>
2022-07-27 16:08:51 -0300

    arregladno filter

--- henry-diamond/src/components/Filter/FilterMaterial.jsx
+++ henry-diamond/src/components/Filter/FilterMaterial.jsx
@@ -9,7 +9,7 @@ export default function FilterMaterial() {
     const dispatch = useDispatch();
     const subcategory = useSelector(state => state.subcategory);
     const [filter, setFilter] = useState({
-        price:"",
+      price:{min:0, max:100000},
         subcategory: "",
     });
     useEffect(() => {
@@ -19,15 +19,18 @@ export default function FilterMaterial() {
         label: "$0 - $1000",
         value:{min: 0, max: 1000} },{
         label:'$1000 - $2000',
-        value:{min: 1000, max:2000}}, {
+        value:{min: 1000, max:2000}},{
         label:'$2000 - $3000',
-        value:{min: 2000, max:3000}}, {
+        value:{min: 2000, max:3000}},{
         label:'$3000 - $4000',
-        value:{min: 3000, max:4000}}, {
+        value:{min: 3000, max:4000}},{
         label:'$4000 - $5000',
-        value: {min: 4000, max:5000}}, {
+        value: {min: 4000, max:5000}},{
         label:'$5000 - $6000',
-        value:{min: 5000, max:6000}}];
+        value:{min: 5000, max:6000}},{ 
+        label:'Todos los precios',
+        value:{min: 0, max: 1000000}}
+        ];
    const product =[]
    const arraycategories = subcategory.map(e=> product.push({label:e.name, value:e.subCategory_id}));
    const handleChange = (e) => {
@@ -49,7 +52,7 @@ export default function FilterMaterial() {
         setFilter({...filter, price: newValue.value})}}
       renderInput={(params) => <TextField {...params} label="POR PRECIOS" />}
       />
-      
+      <br />
     <Autocomplete
       disablePortal
       id="combo-box-demo"
